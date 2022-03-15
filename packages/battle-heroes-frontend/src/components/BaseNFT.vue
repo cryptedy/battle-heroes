e270f23f49d14c91552c6be1b50f6412f2239c12
boo-chang <booandchang@gmail.com>
2022-03-15 13:22:24 +0000

    add NFT parameters

--- packages/battle-heroes-frontend/src/components/BaseNFT.vue
+++ packages/battle-heroes-frontend/src/components/BaseNFT.vue
@@ -1,13 +1,22 @@
 <template>
   <img :src="nft.image_url" :alt="nft.name" width="64" height="64" />
   <p>
+    <span>★★★☆☆</span>
+  </p>
+  <p>
     <strong>{{ nft.name }}</strong>
     -
     <span style="color: red">Rank: #{{ nft.rank }}</span>
     -
     <span style="color: blue">Score: {{ nft.score }}</span>
   </p>
-
+  <ul>
+    <li>HP: {{ Math.floor(Math.random() * 255) }}</li>
+    <li>ATK: {{ Math.floor(Math.random() * 255) }}</li>
+    <li>DEF: {{ Math.floor(Math.random() * 255) }}</li>
+    <li>AGI: {{ Math.floor(Math.random() * 255) }}</li>
+    <li>TYPE: XXX</li>
+  </ul>
   <base-accordion :open="false">
     <template #trigger="scopeProps">
       <a style="color: blue; cursor: pointer">
