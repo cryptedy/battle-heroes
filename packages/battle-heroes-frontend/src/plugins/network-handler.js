926a3957decce36f0cf422e291d0ecac7f3504ac
boo-chang <booandchang@gmail.com>
2022-03-31 16:28:16 +0000

    logout on offline network

--- packages/battle-heroes-frontend/src/plugins/network-handler.js
+++ packages/battle-heroes-frontend/src/plugins/network-handler.js
@@ -1,4 +1,5 @@
 import store from '@/store'
+// import { socket } from './socket'
 
 export default {
   // eslint-disable-next-line no-unused-vars
@@ -13,6 +14,14 @@ export default {
     // eslint-disable-next-line no-unused-vars
     window.addEventListener('offline', event => {
       store.dispatch('network/onOffline')
+
+      // TEST 1
+      if (store.getters['game/isLogin']) {
+        store.dispatch('game/logout')
+      }
+
+      // TEST 2
+      // socket.disconnect()
 
       console.log('The network connection has been lost.')
     })
