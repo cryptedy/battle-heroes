7b56af521c3ff24de582e038c1f0e6ae788f822b
boo-chang <booandchang@gmail.com>
2022-08-29 14:28:03 +0000

    change player sort logic

--- packages/battle-heroes-frontend/src/views/arena/Players.vue
+++ packages/battle-heroes-frontend/src/views/arena/Players.vue
@@ -7,7 +7,6 @@
 <script>
 import { mapGetters } from 'vuex'
 import PlayerList from '@/components/PlayerList'
-import { PLAYER_STATE } from '@/utils/constants'
 
 export default {
   name: 'PlayersView',
@@ -24,12 +23,10 @@ export default {
     sortedPlayers() {
       let sortedPlayers = this.players
 
-      sortedPlayers = sortedPlayers.sort((a, b) => {
-        if (b.state !== PLAYER_STATE.IDLE && a.state === PLAYER_STATE.IDLE)
-          return 1
-        if (b.exp !== a.exp) return b.exp - a.exp
-        return b.socket_ids.length - a.socket_ids.length
-      })
+      sortedPlayers = sortedPlayers.sort((a, b) => b.exp - a.exp)
+      sortedPlayers = sortedPlayers.sort(
+        (a, b) => b.socket_ids.length - a.socket_ids.length
+      )
 
       return sortedPlayers
     }
--- packages/battle-heroes-frontend/src/views/rankings/Total.vue
+++ packages/battle-heroes-frontend/src/views/rankings/Total.vue
@@ -5,6 +5,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import PlayerList from '@/components/PlayerList'
+import { PLAYER_STATE } from '@/utils/constants'
 
 export default {
   name: 'RankingsTotalView',
@@ -21,10 +22,12 @@ export default {
     sortedPlayers() {
       let sortedPlayers = this.players
 
-      sortedPlayers = sortedPlayers.sort((a, b) => b.exp - a.exp)
-      sortedPlayers = sortedPlayers.sort(
-        (a, b) => b.socket_ids.length - a.socket_ids.length
-      )
+      sortedPlayers = sortedPlayers.sort((a, b) => {
+        if (b.state !== PLAYER_STATE.IDLE && a.state === PLAYER_STATE.IDLE)
+          return 1
+        if (b.exp !== a.exp) return b.exp - a.exp
+        return b.socket_ids.length - a.socket_ids.length
+      })
 
       return sortedPlayers
     }
