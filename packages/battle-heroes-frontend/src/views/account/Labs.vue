078c22a073b826bb95c0d748c42ba979bde1f3da
cryptedy <edy.sassolino@gmail.com>
2022-10-15 22:50:36 +0900

    update Labs.vue(incomplete)

--- packages/battle-heroes-frontend/src/views/account/Labs.vue
+++ packages/battle-heroes-frontend/src/views/account/Labs.vue
@@ -12,6 +12,7 @@
   <BaseButton type="primary" @click="mintTokenExp">
     Transfer Exp to Chain
   </BaseButton>
+  <BaseButton type="primary" @click="connect"> Connect Wallet </BaseButton>
   <P>CollectionId:</P>
   <input v-model="colId1" type="text" style="color: #000000" />
   <P>TokenId:</P>
@@ -29,8 +30,9 @@
 <script>
 import { mapGetters } from 'vuex'
 import { NOTIFICATION_TYPE } from '../../utils/constants'
+import { CHAINID_VAULT } from '../../../../battle-heroes-utils/constants'
 //import Moralis from 'moralis/dist/moralis.min.js'
-const Moralis = require('moralis/node')
+//const Moralis = require('moralis/node')
 
 const DEFAULT_MESSAGE = 'Welcome to Labs'
 
@@ -120,7 +122,7 @@ export default {
           console.log('Signature was issued', status, message, payload)
           if (status) {
             // トランザクション発行処理
-            const {
+            /*const {
               startBlockNumber,
               collectionId,
               tokenId,
@@ -128,13 +130,12 @@ export default {
               nonce,
               hash
             } = payload
-
+*/
             //TODO
-            const tx = await Moralis.Web3API.native.send()
 
             //成功した場合の処理
-            if (true) {
-            }
+            //            if (true) {
+            //            }
 
             this.addNotification({
               message,
@@ -151,7 +152,56 @@ export default {
       )
       this.isCalled = true
       this.message = 'AddExp caled!'
+    },
+
+    async connect() {
+      if (window.ethereum) {
+        await handleEthereum()
+      } else {
+        window.addEventListener('ethereum#initialized', await handleEthereum, {
+          once: true
+        })
+
+        // If the event is not dispatched by the end of the timeout,
+        // the user probably doesn't have MetaMask installed.
+        setTimeout(await handleEthereum, 3000) // 3 seconds
+      }
     }
   }
+}
+const handleEthereum = async () => {
+  const { ethereum } = window
+  if (ethereum && ethereum.isMetaMask) {
+    console.log('Ethereum successfully detected!')
+    // Access the decentralized web!
+    try {
+      const acccounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      })
+      if (acccounts.length > 0) {
+        console.log(acccounts[0])
+        // チェーンIDの切り替え要求　ChainListがない場合に追加を促すう処理は未実装
+        if (ethereum.chainid != CHAINID_VAULT) {
+          await ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: toHex(CHAINID_VAULT) }]
+          })
+        }
+      }
+    } catch (err) {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // ユーザーが接続を拒否するとここに来ます
+        console.log('Please connect to MetaMask.')
+      } else {
+        console.error(err)
+      }
+    }
+  } else {
+    console.log('Please install MetaMask!')
+  }
+}
+function toHex(v) {
+  return '0x' + ('0000' + v.toString(16)).substr(-4)
 }
 </script>
